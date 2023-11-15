48379bb29a3f971f96b8c89f73e82dfc75ab26f7
skar_penelope <penelopeskarlate@gmail.com>
2023-11-15 16:17:13 -0300

    1511

--- soaw/Assets/Scripts/Player.cs
+++ soaw/Assets/Scripts/Player.cs
@@ -14,9 +14,13 @@ public class Player : MonoBehaviour
     //pulo duplo
     public bool duploPulo;
     public bool isJumping;
+    
+    //animações
+    private Animator anim;
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
     
     void Update()
@@ -29,29 +33,60 @@ public class Player : MonoBehaviour
     {
         Vector3 movimento = new Vector3(Input.GetAxis("Horizontal"), 0f,0f); //input; ao apertar a ou d, o personagem vai movimentar no eixo x, eixo y e z estão sem nada.
         transform.position += movimento * Time.deltaTime * velocidade; //posição vai ser somado ao movimento que pode ser +1, para direita, ou -1, para esquerda.
+
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            anim.SetBool("walk_direita", true);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            anim.SetBool("walk_direita", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+        
+        if (Input.GetAxis("Horizontal") == 0f)
+        {
+            anim.SetBool("walk_direita", false);
+        }
+        
     }
 
     public void Pulo()
     {
-        if (Input.GetButtonDown("Jump"))  //se o botão jump(espaço) for apertado, vai executar.
+        if (Input.GetButtonDown("Jump"))  //se o botão jump(espaço) for apertado, vai executar. Analisa se o isJumping está verdadeiro.
         {
-            rig.AddForce(new Vector2(0f, Jumpforce), ForceMode2D.Impulse);
+            if (!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, Jumpforce), ForceMode2D.Impulse);
+                duploPulo = true;
+            }
+            else
+            {
+                if (duploPulo)
+                {
+                    rig.AddForce(new Vector2(0f, Jumpforce), ForceMode2D.Impulse);
+                    duploPulo = false;
+                }
+            }
+            //rig.AddForce(new Vector2(0f, Jumpforce), ForceMode2D.Impulse);
         }
     }
     
     private void OnCollisionEnter2D(Collision2D Colisor)
     {
-        if (Colisor.gameObject.layer == 6)
+        if (Colisor.gameObject.layer == 6) // se o game object estiver tocando no layer 6, vai executar.
         {
-            
+            isJumping = false;
         }
     }
     
-    private void OnCollisionExit2D(Collision2D Colisor)
+    private void OnCollisionExit2D(Collision2D Colisor) // se o game object estiver tocando no layer 6, vai executar.
     {
         if (Colisor.gameObject.layer == 6)
         {
-            
+            isJumping = true;
         }
         
     }
